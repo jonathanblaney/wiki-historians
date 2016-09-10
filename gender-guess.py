01aeb9bd2c532be717fbebd6c7a3e462086069d4
Jonathan Blaney <jonathan.blaney@sas.ac.uk>
2016-09-10 16:27:10 +0100

    tested gender guessing on directory, looks OK

--- gender-guess.py
+++ gender-guess.py
@@ -1,21 +1,21 @@
 import re
-f = open('Tony_Judt.txt')
-text = f.read()
-f.close()
+import glob
 
-he = re.findall(r"\bhe\b", text, re.I)
-she = re.findall(r"\bshe\b", text, re.I)
-his = re.findall(r"\bhis\b", text, re.I)
-her = re.findall(r"\bher\b", text, re.I)
+for name in glob.glob("*-raw.txt"):
+    f = open(name)
+    wikihistorian = f.read()
+    f.close()
 
-male_count = len(he) + len(his)
-female_count = len(she) + len(her)
 
-print male_count
-print female_count
+    he = re.findall(r"\bhe\b", wikihistorian, re.I)
+    she = re.findall(r"\bshe\b", wikihistorian, re.I)
+    his = re.findall(r"\bhis\b", wikihistorian, re.I)
+    her = re.findall(r"\bher\b", wikihistorian, re.I)
 
-if male_count > female_count: #assume equal numbers = woman
-    print("man!")
-else:
-    print("woman!")
+    male_count = len(he) + len(his)
+    female_count = len(she) + len(her)
 
+    if male_count > female_count: #assume equal numbers = woman
+        print(name+" guessing ... man. (male "+str(male_count)+ "/female "+str(female_count)+")")
+    else:
+        print(name+" guessing ... woman. (male "+str(male_count)+ "/female "+str(female_count)+")")
